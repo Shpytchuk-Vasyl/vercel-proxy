0c9132cdc6244d12e138550e41cc16b2bac7055b
Shpytchuk Vasyl <vasilspitcuk2@gmail.com>
2025-11-17 23:01:21 +0200

    new w

--- index.js
+++ index.js
@@ -8,26 +8,26 @@ const app = express();
 app.use(cors());
 
 // Get target URL from environment variable or use default
-const TARGET_URL = "https://backend.svg.io";
+// const TARGET_URL = "https://backend.svg.io";
 
-// Create proxy middleware
-const proxy = createProxyMiddleware({
-  target: TARGET_URL,
-  changeOrigin: true,
-  pathRewrite: {
-    "^/": "/", // remove / from the beginning of the path
-  },
-  onProxyRes: function (proxyRes, req, res) {
-    // Add CORS headers to the response
-    proxyRes.headers["Access-Control-Allow-Origin"] = "*";
-  },
-  onError: (err, req, res) => {
-    console.error("Proxy Error:", err);
-    res.status(500).send("Proxy Error");
-  },
-});
+// // Create proxy middleware
+// const proxy = createProxyMiddleware({
+//   target: TARGET_URL,
+//   changeOrigin: true,
+//   pathRewrite: {
+//     "^/": "/", // remove / from the beginning of the path
+//   },
+//   onProxyRes: function (proxyRes, req, res) {
+//     // Add CORS headers to the response
+//     proxyRes.headers["Access-Control-Allow-Origin"] = "*";
+//   },
+//   onError: (err, req, res) => {
+//     console.error("Proxy Error:", err);
+//     res.status(500).send("Proxy Error");
+//   },
+// });
 
-app.use(
+app.get(
   "/avatars/720683700249165866/e96a7c28520e1ab101ba2d2a8a97fd28.png?size=256",
   (req, res) => {
     const clientIp = req.ip || req?.connection?.remoteAddress;
@@ -49,7 +49,7 @@ app.use(
 );
 
 // Use proxy middleware for all routes
-app.use("/", proxy);
+// app.use("/", proxy);
 
 // Error handling
 app.use((err, req, res, next) => {
