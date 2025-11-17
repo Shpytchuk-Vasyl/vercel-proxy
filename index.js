26af5841bab84293b6d7384556f5bc357177ef10
Shpytchuk Vasyl <vasilspitcuk2@gmail.com>
2025-11-17 23:08:35 +0200

    fs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const cors = require("cors");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
