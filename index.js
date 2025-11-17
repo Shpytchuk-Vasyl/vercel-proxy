a5502b6e95fe4970e90600228c6b4de16386b56e
Shpytchuk Vasyl <vasilspitcuk2@gmail.com>
2025-11-17 23:13:05 +0200

    revert

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const cors = require("cors");
-const path = require("path");
-const fs = require("fs");
 
 const app = express();
 
@@ -10,48 +8,27 @@ const app = express();
 app.use(cors());
 
 // Get target URL from environment variable or use default
-// const TARGET_URL = "https://backend.svg.io";
+const TARGET_URL = "https://backend.svg.io";
 
-// // Create proxy middleware
-// const proxy = createProxyMiddleware({
-//   target: TARGET_URL,
-//   changeOrigin: true,
-//   pathRewrite: {
-//     "^/": "/", // remove / from the beginning of the path
-//   },
-//   onProxyRes: function (proxyRes, req, res) {
-//     // Add CORS headers to the response
-//     proxyRes.headers["Access-Control-Allow-Origin"] = "*";
-//   },
-//   onError: (err, req, res) => {
-//     console.error("Proxy Error:", err);
-//     res.status(500).send("Proxy Error");
-//   },
-// });
-
-app.get(
-  "/avatars/720683700249165866/e96a7c28520e1ab101ba2d2a8a97fd28.png",
-  (req, res) => {
-    const clientIp = req.ip || req?.connection?.remoteAddress;
-    const cookiesHeader = req.headers.cookie || "";
-    const parsedCookies = req.cookies || {};
-
-    const log = `IP=${clientIp} | CookieHeader="${cookiesHeader}" | Parsed=${JSON.stringify(
-      parsedCookies
-    )}`;
-    console.log(log);
-
-    const imgPath = path.join(__dirname, "public", "avatar.jpg");
-    if (fs.existsSync(imgPath)) {
-      res.sendFile(imgPath);
-    } else {
-      res.status(404).send("avatar not found");
-    }
-  }
-);
+// Create proxy middleware
+const proxy = createProxyMiddleware({
+  target: TARGET_URL,
+  changeOrigin: true,
+  pathRewrite: {
+    "^/": "/", // remove / from the beginning of the path
+  },
+  onProxyRes: function (proxyRes, req, res) {
+    // Add CORS headers to the response
+    proxyRes.headers["Access-Control-Allow-Origin"] = "*";
+  },
+  onError: (err, req, res) => {
+    console.error("Proxy Error:", err);
+    res.status(500).send("Proxy Error");
+  },
+});
 
 // Use proxy middleware for all routes
-// app.use("/", proxy);
+app.use("/", proxy);
 
 // Error handling
 app.use((err, req, res, next) => {
@@ -61,11 +38,6 @@ app.use((err, req, res, next) => {
 
 // Start server
 const port = process.env.PORT || 3000;
-
-// server.js
-const cookieParser = require("cookie-parser");
-
-app.use(cookieParser());
 
 app.listen(port, () => {
   console.log(`Proxy server running on port ${port}`);
